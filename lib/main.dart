6a18eb315adb01f7906858c27785f06d8fb3ca19
Odilon Damasceno <storckskey@gmail.com>
2020-04-07 17:11:20 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PWB());
+}
+
+class PWB extends StatefulWidget {
+  @override
+  _PWBState createState() => _PWBState();
+}
+
+class _PWBState extends State<PWB> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
