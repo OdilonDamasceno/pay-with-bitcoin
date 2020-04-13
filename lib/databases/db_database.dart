75a37de8f3a5ea74e773f6dfd7e3d556cf8c3086
Odilon Damasceno <storckskey@gmail.com>
2020-04-13 10:18:24 -0300

    feat: connection with database

--- lib/databases/db_database.dart
+++ lib/databases/db_database.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
+import 'package:pay_with_bitcoin/models/item_model.dart';
 import 'package:sqflite/sqflite.dart';
 import '../models/user_model.dart';
 
@@ -12,10 +13,14 @@ class DB {
   final String userTable = "userTable";
   final String colId = "id";
   final String colAddress = "address";
-  final String productsList = "productsList";
+  final String productTable = "productTable";
+  final String productName = "name";
+  final String productValue = "price";
+  final String productDescription = "description";
+  final String productImage = "image";
 
   static Database _db;
-  Future<Database> get db async { 
+  Future<Database> get db async {
     // If _db exists in memory
     if (_db != null) {
       return _db;
@@ -34,8 +39,10 @@ class DB {
 
   void _onCreate(Database db, int version) async {
     await db.execute(
-      'CREATE TABLE $userTable($colId INTEGER PRIMARY KEY,"$colAddress" TEXT, $productsList TEXT)',
-    );
+        'CREATE TABLE $userTable($colId INTEGER PRIMARY KEY,"$colAddress" TEXT)');
+    await db.execute(
+        'CREATE TABLE $productTable($colId INTEGER PRIMARY KEY, "$productName" TEXT,'
+        ' "$productDescription" TEXT, "$productImage" TEXT, "$productValue" TEXT)');
   }
 
   Future<int> insertUser(User user) async {
@@ -43,6 +50,17 @@ class DB {
 
     int res = await dbClient.insert("$userTable", user.toMap());
     return res;
+  }
+
+  Future<int> insertNewItem(Item item) async {
+    var dbClient = await db;
+    int res = await dbClient.insert("$productTable", item.toMap());
+    return res;
+  }
+
+  Future<List> getProduct() async {
+    var dbClient = await db;
+    return (await dbClient.rawQuery("SELECT * FROM $productTable")).toList();
   }
 
   Future<List> getUsers() async {
@@ -54,7 +72,13 @@ class DB {
   Future<int> getCount() async {
     var dbClient = await db;
     return Sqflite.firstIntValue(
-        await dbClient.rawQuery("SELECT COUNT(*) from $userTable"));
+        await dbClient.rawQuery("SELECT COUNT(*) FROM $userTable"));
+  }
+
+  Future<int> getLength() async {
+    var dbClient = await db;
+    return Sqflite.firstIntValue(
+        await dbClient.rawQuery("SELECT COUNT(*) FROM $productTable"));
   }
 
   Future<User> getUser(int id) async {
@@ -65,10 +89,24 @@ class DB {
     return new User.fromMap(res.first);
   }
 
+  Future<Item> getItem(int id) async {
+    var dbCLient = await db;
+    var res = await dbCLient
+        .rawQuery("SELECT * FROM $productTable WHERE $colId = $id");
+    if (res.length == 0) return null;
+    return new Item.fromMap(res.first);
+  }
+
   Future<int> deleteUser(int id) async {
     var dbClient = await db;
     return await dbClient
         .delete(userTable, where: "$colId = ?", whereArgs: [id]);
+  }
+
+  Future<int> deleteItem(int id) async {
+    var dbClient = await db;
+    return await dbClient
+        .delete(productTable, where: "$colId =?", whereArgs: [id]);
   }
 
   Future<int> editUser(User user) async {
