c508e435714061543b4ec0a60d6e9f144674af52
Odilon Damasceno <storckskey@gmail.com>
2020-04-13 10:19:50 -0300

    fix: removed variable productsList

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,26 +1,24 @@
 class User {
   int _id;
   String _address;
-  String _productsList;
+
   User(
     this._address,
-    this._productsList,
   );
 
   User.map(dynamic obj) {
     this._address = obj['address'];
     this._id = obj['id'];
-    this._productsList = obj['productList'];
   }
 
   String get address => _address;
-  String get productsList => _productsList;
+
   int get id => _id;
 
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
     map['address'] = _address;
-    map['productsList'] = _productsList;
+
     if (id != null) {
       map['id'] = _id;
     }
@@ -30,6 +28,5 @@ class User {
   User.fromMap(Map<String, dynamic> map) {
     this._address = map['address'];
     this._id = map['id'];
-    this._productsList = map['productsList'];
   }
 }
